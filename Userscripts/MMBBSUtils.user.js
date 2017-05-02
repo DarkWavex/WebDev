8b2d2171194d297b504b5ebeaa2039e6fc454a31
Yannick Fricke <yfricke@qnc.de>
2017-05-02 11:03:32 +0200

    fix

--- Userscripts/MMBBSUtils.user.js
+++ Userscripts/MMBBSUtils.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MMBBSUtils
 // @namespace    https://github.com/TheCharmingCthulhu/WebDev/raw/master/Userscripts/
-// @version      1.4.8
+// @version      1.4.9
 // @description  Multimedia BBS Berufsschule - Utilities & Extensions.
 // @author       TheCharmingCthulhu,littlejak20
 // @match        http://www.mmbbs.de/*
