f29956309dda407c7aab6c757ac3149654a21e45
david.dembinski.cthulhu <darkvplayer@gmail.com>
2017-07-24 23:14:17 +0200

    Updated "QMP3" Version number... :P

--- Userscripts/QMP3.user.js
+++ Userscripts/QMP3.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/TheCharmingCthulhu/WebDev/raw/master/Userscripts/
 // @require      http://code.jquery.com/jquery-latest.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @version      0.7.1
+// @version      0.7.2
 // @description  Youtube2MP3 extension.
 // @author       TheCharmingCthulhu
 // @match        https://www.youtube.com/*
