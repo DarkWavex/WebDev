d4fdd5a1886482454343dfeefb7709d94cdbe34d
David Dembinski <darkvplayer@gmail.com>
2017-07-31 18:25:31 +0200

    Updated QMP3:  + Fixed issues with the new youtube layout...  + Now actually checks all ajax request on youtube and sets the correct src="" url on the embedded iframe...  + Contains a updateSource(source) method for swapping out the src url of the iframe...  + Compatibility between the old & new layout..  + Bugfixes...

--- Userscripts/QMP3.user.js
+++ Userscripts/QMP3.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/TheCharmingCthulhu/WebDev/raw/master/Userscripts/
 // @require      http://code.jquery.com/jquery-latest.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @version      0.7.2
+// @version      0.7.3
 // @description  Youtube2MP3 extension.
 // @author       TheCharmingCthulhu
 // @match        https://www.youtube.com/*
@@ -13,11 +13,25 @@
 // @updateURL 	 https://github.com/TheCharmingCthulhu/WebDev/raw/master/Userscripts/QMP3.user.js
 // ==/UserScript==
 
-waitForKeyElements("#meta-skeleton", onPageLoad);
+waitForKeyElements("#search-form", onPageLoad);
+waitForKeyElements("#watch-header", onPageLoad);
 
-function onPageLoad(jNode){
-    var video_id = window.location.search.substring(1).split('=')[1];
-    var frame_code = '<iframe id="download" style="width:100%;height:40px;border:0;overflow:hidden;margin-top:8px;" scrolling="no" src="//www.youtubeinmp3.com/widget/button/?video=https://www.youtube.com/watch?v=' + video_id + '"&color=e62117>';
+function onPageLoad(jNode) {
+    console.log();
+    var source = "";
+    var style = (jNode[0].id === 'watch-header') ? "width:100%;height:40px;border:0;overflow:hidden;margin-top:8px;" : "height:40px;border:0;overflow:hidden;margin-left:10px;";
+    var frame_code = '<iframe id="download" style="' + style + '" scrolling="no" color=e62117>';
     if ($('#download').length === 0)
         jNode.append(frame_code);
+    document.body.addEventListener('load', function(e) {
+        if (source !== window.location.href) {
+            source = window.location.href;
+            updateSource(window.location.href);
+        }
+    }, true);
+}
+
+function updateSource(source) {
+    if ($('#download').length > 0)
+        $('#download').attr('src', "//www.youtubeinmp3.com/widget/button/?video=" + source); //https://www.youtube.com/watch?v=" + source);
 }
