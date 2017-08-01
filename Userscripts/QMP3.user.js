6d08c92bf9efab8d14d8b01bd6c37bfaee9dcdfc
David Dembinski <darkvplayer@gmail.com>
2017-08-01 11:02:28 +0200

    Update QMP3:   + Fixed various bugs...   + Added Regex check for Video URL   + Optimized iframe...

--- Userscripts/QMP3.user.js
+++ Userscripts/QMP3.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/TheCharmingCthulhu/WebDev/raw/master/Userscripts/
 // @require      http://code.jquery.com/jquery-latest.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @version      0.7.3
+// @version      0.7.4
 // @description  Youtube2MP3 extension.
 // @author       TheCharmingCthulhu
 // @match        https://www.youtube.com/*
@@ -17,18 +17,21 @@ waitForKeyElements("#search-form", onPageLoad);
 waitForKeyElements("#watch-header", onPageLoad);
 
 function onPageLoad(jNode) {
-    console.log();
     var source = "";
-    var style = (jNode[0].id === 'watch-header') ? "width:100%;height:40px;border:0;overflow:hidden;margin-top:8px;" : "height:40px;border:0;overflow:hidden;margin-left:10px;";
+    var style = (jNode[0].id === 'watch-header') ? "width:100%;height:40px;margin-top:8px;" : "width:25%;height:40px;margin-left:10px;";
     var frame_code = '<iframe id="download" style="' + style + '" scrolling="no" color=e62117>';
     if ($('#download').length === 0)
         jNode.append(frame_code);
     document.body.addEventListener('load', function(e) {
-        if (source !== window.location.href) {
+        if (source !== window.location.href && isValidURL(window.location.href)) {
             source = window.location.href;
             updateSource(window.location.href);
         }
     }, true);
+}
+
+function isValidURL(url) {
+    return new RegExp(/http(?:s?):\/\/(?:www|youtu?).*\/(?:watch\?v=([A-z0-9-]{11})|([A-z0-9-]{11}) ?)/).test(url);
 }
 
 function updateSource(source) {
