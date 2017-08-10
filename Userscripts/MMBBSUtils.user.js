580d8e29e62edc0a2a3caf2f2c05ddf92e7a8bec
David Dembinski <darkvplayer@gmail.com>
2017-08-10 10:10:20 +0200

    Update MMBBSUtils:   + "Blockplan" update from 2017-2018...

--- Userscripts/MMBBSUtils.user.js
+++ Userscripts/MMBBSUtils.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MMBBSUtils
 // @namespace    https://github.com/TheCharmingCthulhu/WebDev/raw/master/Userscripts/
-// @version      1.5.1
+// @version      1.5.2
 // @description  Multimedia BBS Berufsschule - Utilities & Extensions.
 // @author       TheCharmingCthulhu,littlejak20
 // @match        http://www.mmbbs.de/*
@@ -14,18 +14,18 @@
 (function() {
     'use strict';
 
-    var classID  = 'c00026';
+    var classID = 'c00026';
     var proxyID = 'c00030';
     var proxyListID = 'w00030';
-    var blockPlan = 'http://www.mmbbs.de/uploads/media/IT-Blockkalender_2016_2017.pdf';
+    var blockPlan = 'http://www.mmbbs.de/uploads/media/2017-2018_IT-Blockplan_2016-11-18.pdf';
 
     $('head').append("<style>.news-list-item:after{content: ''; display: table; clear: both;}</style>");
 
     Date.prototype.getWeek = function() {
-        var target  = new Date(this.valueOf());
-        var dayNr   = (this.getDay() + 6) % 7;
+        var target = new Date(this.valueOf());
+        var dayNr = (this.getDay() + 6) % 7;
         target.setDate(target.getDate() - dayNr + 3);
-        var jan4    = new Date(target.getFullYear(), 0, 4);
+        var jan4 = new Date(target.getFullYear(), 0, 4);
         var dayDiff = (target - jan4) / 86400000;
         var weekNr = 1 + Math.ceil(dayDiff / 7);
         weekNr -= 1;
@@ -43,13 +43,13 @@
     menu += '<a href="">Werkzeuge</a>';
     menu += '<ul>';
     menu += '<li>';
-    menu += '<a class="tools" href="http://stundenplan.mmbbs.de/plan1011/klassen/' + (today.getWeek() < 10 ? "0" : "") + today.getWeek() + '/c/' + classID +'.htm">Stundenplan Woche ' + (today.getWeek() < 10 ? "0" : "") + today.getWeek() + '</a>';
+    menu += '<a class="tools" href="http://stundenplan.mmbbs.de/plan1011/klassen/' + (today.getWeek() < 10 ? "0" : "") + today.getWeek() + '/c/' + classID + '.htm">Stundenplan Woche ' + (today.getWeek() < 10 ? "0" : "") + today.getWeek() + '</a>';
     menu += '</li>';
     menu += '<li>';
-    menu += '<a class="tools" href="http://stundenplan.mmbbs.de/plan1011/ver_kla/' + (today.getWeek() < 10 ? "0" : "") + today.getWeek() + '/c/' + proxyID +'.htm">Vertretungsplan Woche ' + (today.getWeek() < 10 ? "0" : "") + today.getWeek() + '</a>';
+    menu += '<a class="tools" href="http://stundenplan.mmbbs.de/plan1011/ver_kla/' + (today.getWeek() < 10 ? "0" : "") + today.getWeek() + '/c/' + proxyID + '.htm">Vertretungsplan Woche ' + (today.getWeek() < 10 ? "0" : "") + today.getWeek() + '</a>';
     menu += '</li>';
     menu += '<li>';
-    menu += '<a class="tools" href="http://stundenplan.mmbbs.de/plan1011/ver_kla/' + (today.getWeek() < 10 ? "0" : "") + today.getWeek() + '/w/' + proxyListID +'.htm">Vertretungsplan Liste Woche ' + (today.getWeek() < 10 ? "0" : "") + today.getWeek() + '</a>';
+    menu += '<a class="tools" href="http://stundenplan.mmbbs.de/plan1011/ver_kla/' + (today.getWeek() < 10 ? "0" : "") + today.getWeek() + '/w/' + proxyListID + '.htm">Vertretungsplan Liste Woche ' + (today.getWeek() < 10 ? "0" : "") + today.getWeek() + '</a>';
     menu += '</li>';
     menu += '<li>';
     menu += '<a class="tools" href="' + blockPlan + '">Blockplan: 2016 - 2017</a>';
@@ -59,10 +59,9 @@
 
     $(".menu").append(menu);
     //$("#mitte").html('<iframe style="width: 100%; height: 5000px; border: 0; overflow: hidden;" src="http://stundenplan.mmbbs.de/plan1011/ver_kla/' + (today.getWeek() < 10 ? "0" : "") + today.getWeek() + '/c/' + proxyID +'.htm""></iframe>');
-    
+
     $(".tools").on("click", function(e) {
         e.preventDefault();
-        $("#mitte").html('<iframe style="width: 100%; height: 5000px; border: 0; overflow: hidden;" src="'+$(this).attr('href')+'"></iframe>');
+        $("#mitte").html('<iframe style="width: 100%; height: 5000px; border: 0; overflow: hidden;" src="' + $(this).attr('href') + '"></iframe>');
     });
 })();
-
